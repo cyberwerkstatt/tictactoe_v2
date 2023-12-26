2a8af1980a97050eecf96755f9ff8b55852ebc40
DieCyberwerkstatt <seslija.sasha@gmail.com>
2023-12-26 21:19:31 +0100

    implemented functions

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { routes } from '../app.routes';
 import { Route, Router, RouterOutlet } from '@angular/router';
+import { GameMainComponent } from '../game-main/game-main.component';
+import { CommonModule } from '@angular/common';
+
 
 
 
 @Component({
   selector: 'app-startscreen',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GameMainComponent, CommonModule],
   templateUrl: './startscreen.component.html',
   styleUrl: './startscreen.component.scss'
 })
 export class StartscreenComponent implements OnInit {
+
+  view:boolean = false;
+  viewStart:boolean = true;
 
   // Injizieren des Router-Services
   constructor(private router: Router) { }
@@ -23,6 +29,8 @@ export class StartscreenComponent implements OnInit {
   loadGame() {
     // Verwendung der navigate-Methode des Router-Services
     this.router.navigate(['/Game']);
+    this.view = true;
+    this.viewStart = false;
   }
 
 }
